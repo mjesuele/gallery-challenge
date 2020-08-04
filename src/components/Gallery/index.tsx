a18307ce1b04ad25066080d35d05d1c8ca6f82e7
Matt Jesuele <matt@mvj.fyi>
2020-08-04 13:46:34 -0700

    Initial commit with TypeScript and CSS modules

---
+++ src/components/Gallery/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import css from "./index.module.css";
+
+type Props = {};
+
+const Gallery = () => {
+  return <div className={css.gallery}></div>;
+};
+
+export default Gallery;
